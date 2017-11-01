8623bb3685cc9c5c47f88d8c6c83cc6fa38f2008
BigTonni <shandm85@gmail.com>
2017-11-01 12:36:24 +0200

    change data

--- amazon-scripts/test-amazon.php
+++ amazon-scripts/test-amazon.php
@@ -16,16 +16,16 @@ $s3 = new S3Client([
     'region'  => 'eu-central-1'
 ]);
 
-$access = 'AKIAICVMD7QV2EKUFA3Q';
-$secret = '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';
+$access = 'XXX';
+$secret = 'YYY';
 
 $s3Client = S3Client::factory(array('key' => $access, 'secret' => $secret));
 
 /*// Instantiate the S3 client with your AWS credentials
 $s3 = new S3Client::factory(array(
     'credentials' => array(
-        'key'    => 'AKIAICVMD7QV2EKUFA3Q',
-        'secret' => 'lHbep+kvYeqg5xs5e6hxHO0wrAjpDmqzEsxrOVVy',
+        'key'    => 'XXX',
+        'secret' => 'YYY',
     ),
 	//'Bucket' => 'artbeef'
 ));
